24ded7bbb52ec54ccbac46e1adebe577a9550a0b
Sprinthub <sprinthub@sprinthublion.local>
2019-02-12 10:20:12 +0100

    Initial Commit

---
+++ WordGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGame
+//
+//  Created by Sprinthub on 12/02/2019.
+//  Copyright © 2019 Sprinthub Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
